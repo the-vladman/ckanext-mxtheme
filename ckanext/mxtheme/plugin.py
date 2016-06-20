33ff63cda45bd6168c6a424bc945e7a416e78615
Frank <akura11.tt@gmail.com>
2016-06-20 14:03:37 -0500

    Generando urls con contexto

--- ckanext/mxtheme/plugin.py
+++ ckanext/mxtheme/plugin.py
@@ -146,8 +146,8 @@ def url_for(*args, **kw):
     if kw.get('qualified', False):
         kw['protocol'], kw['host'] = get_site_protocol_and_host()
     my_url = _routes_default_url_for(*args, **kw)
-    kw['__ckan_no_root'] = no_root
-    return _add_i18n_to_url(my_url, locale=locale, __ckan_no_root=True, **kw)
+    kw['__ckan_no_root'] = True
+    return _add_i18n_to_url(my_url, locale=locale, **kw)
 
 
 class MxthemePlugin(plugins.SingletonPlugin):
